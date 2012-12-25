1d189e6ae4610fa4572970207f39b614c65d8ebe
Marc Rosser <crakeronmarc@caramail.com>
2012-12-25 10:00:50 +0100

    first upload

---
+++ Eclipse Source/src/com/crakeron/stockmod/settings/power_activity.java
@@ -0,0 +1,13 @@
+package com.crakeron.stockmod.settings;
+
+public class power_activity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
