dd94c5ccc163498d079a5ee5124275ef8f31601b
Marc Rosser <crakeronmarc@caramail.com>
2012-12-25 14:25:28 +0100

    Edited actionbar menu

--- Eclipse Source/gen/com/crakeron/stockmod/settings/R.java
+++ Eclipse Source/gen/com/crakeron/stockmod/settings/R.java
@@ -16,7 +16,8 @@ public final class R {
     public static final class id {
         public static final int Button_recovery=0x7f070001;
         public static final int Reboot_title=0x7f070000;
-        public static final int menu_settings=0x7f070002;
+        public static final int changelog=0x7f070002;
+        public static final int item1=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -25,7 +26,10 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int app_info=0x7f040006;
+        public static final int app_info_condensed=0x7f040007;
         public static final int app_name=0x7f040000;
+        public static final int changelog=0x7f040005;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int reboot_recovery=0x7f040004;
